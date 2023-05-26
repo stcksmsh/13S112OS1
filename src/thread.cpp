e092f49bbf090799396f37054f64995c913672cd
stcksmsh <vukicevickosta@gmail.com>
2023-05-26 05:09:05 +0200

    and now i used csrw instead of mv

--- src/thread.cpp
+++ src/thread.cpp
@@ -44,7 +44,7 @@ void thread::dispatch(uint64 pc){
     if(oldThread!=nullptr && !oldThread->finished && !oldThread->blocked)Scheduler::put(running);
     running = Scheduler::get();
     if(running == nullptr){
-        __asm__ volatile ("csrw ra, %0" :: "r"(mainAR));
+        __asm__ volatile ("mv ra, %0" :: "r"(mainAR));
         return;
     }
     switchContext(oldThread==nullptr?nullptr:&(oldThread->context), &(running->context));
