670a41243f6e4185814f2438a922a7970ad22047
stcksmsh <vukicevickosta@gmail.com>
2023-05-26 09:34:12 +0200

    Hello world! but in usermain

--- h/console.h
+++ h/console.h
@@ -1,14 +1,7 @@
 #ifndef CONSOLEHEADERFILE
 #define CONSOLEHEADERFILE
 
-#include "../lib/hw.h"
-#include "../lib/console.h"
+#include "syscall_c.hpp"
 
-void printf(char* text){
-    while(*text!='\0'){
-        __putc(text[0]);
-        text++;
-    }
-}
 
 #endif
--- src/usermain.cpp
+++ src/usermain.cpp
@@ -1,20 +1,17 @@
-#include "../h/console.h"
 #include "../h/syscall_c.hpp"
 
 void usermain(){
-    char* s = (char*)mem_alloc(13);
-    s[0] = 'H';
-    s[1] = 'e';
-    s[2] = 'l';
-    s[3] = 'l';
-    s[4] = 'o';
-    s[5] = ' ';
-    s[6] = 'w';
-    s[7] = 'o';
-    s[8] = 'r';
-    s[9] = 'l';
-    s[10] = 'd';
-    s[11] = '!';
-    s[11] = '\n';
-    printf(s);
+    putc('H');
+    putc('e');
+    putc('l');
+    putc('l');
+    putc('o');
+    putc(' ');
+    putc('w');
+    putc('o');
+    putc('r');
+    putc('l');
+    putc('d');
+    putc('!');
+    putc('\n');
 }
