8f78d5b05fa899166b234e2e97c29de9daefcf81
stcksmsh <vukicevickosta@gmail.com>
2023-05-26 09:39:46 +0200

    now multiple threads in usermain!!!

--- src/usermain.cpp
+++ src/usermain.cpp
@@ -1,7 +1,22 @@
 #include "../h/syscall_c.hpp"
 #include "../h/usermain.h"
 
+void bullshit(void* arg){
+    putc('B');
+    putc('u');
+    putc('l');
+    putc('l');
+    putc('s');
+    putc('h');
+    putc('i');
+    putc('t');
+    putc('!');
+    // thread_exit();
+}
+
 void usermain(){
+    thread_t handle;
+    thread_create(&handle, bullshit, nullptr);
     putc('H');
     putc('e');
     putc('l');
@@ -14,5 +29,21 @@ void usermain(){
     putc('l');
     putc('d');
     putc('!');
+    putc('\n');
+    thread_dispatch();
+    putc('H');
+    putc('o');
+    putc('w');
+    putc(' ');
+    putc('d');
+    putc('a');
+    putc('r');
+    putc('e');
+    putc(' ');
+    putc('y');
+    putc('u');
+    putc('o');
+    putc('!');
+    putc('?');
     putc('\n');
 }
