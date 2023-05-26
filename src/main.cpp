9722cd575b5092fd432ade78a64be62922f322a0
stcksmsh <vukicevickosta@gmail.com>
2023-05-26 02:51:55 +0200

    why cant I mv a value into sp???

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,8 @@ void doFunc(){
 
 void main(){
     __asm__ volatile ("csrw stvec, %0" : :  "r"(&trap));
+    uint64 sp = 16;
+    __asm__ volatile ("mv sp, %0" : "r"(sp));
     thread_t handle;
     address = (uint64)helloWorld;
     // doFunc();
