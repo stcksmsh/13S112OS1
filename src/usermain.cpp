e3f28041aa9cf87a900fc590f46ce79e691b60d9
stcksmsh <vukicevickosta@gmail.com>
2023-05-26 09:32:14 +0200

    ?

--- src/usermain.cpp
+++ src/usermain.cpp
@@ -1,6 +1,20 @@
 #include "../h/console.h"
-#include "../h/thread.h"
+#include "../h/syscall_c.hpp"
 
 void usermain(){
-    printf("Hello World!\0");
+    char* s = (char*)mem_alloc(13);
+    s[0] = 'H';
+    s[1] = 'e';
+    s[2] = 'l';
+    s[3] = 'l';
+    s[4] = 'o';
+    s[5] = ' ';
+    s[6] = 'w';
+    s[7] = 'o';
+    s[8] = 'r';
+    s[9] = 'l';
+    s[10] = 'd';
+    s[11] = '!';
+    s[11] = '\n';
+    printf(s);
 }
