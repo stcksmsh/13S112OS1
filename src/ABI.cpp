72cd2f9b955be92b3474137f73dd4114e6cc0ff1
stcksmsh <vukicevickosta@gmail.com>
2023-05-30 01:50:37 +0200

    .....

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -141,7 +141,7 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
 
         sstatusWrite(sstatus);
         __asm__ volatile ("csrw sepc, %0" : : "r" (sepc + 4));
-        // sipBitClear(1);
+        sipBitClear(1);
     }
     else if (scause == 0x8000000000000001UL)
     {
@@ -149,7 +149,7 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
         if(!thread::running->live()){/// it has run for longer than its alloted time slice
             thread::dispatch();
         }
-        // sipBitClear(1);
+        sipBitClear(1);
 
     }
     else if (scause== 0x8000000000000009UL)
