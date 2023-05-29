c51504c0990d00cf48aa141fa3834759f92b6087
stcksmsh <vukicevickosta@gmail.com>
2023-05-30 01:24:41 +0200

    attempting to save s0..11 when switching context

--- src/thread.cpp
+++ src/thread.cpp
@@ -86,44 +86,38 @@ void thread::dispatch(){
 }
 
 void thread::switchContext(contextWrapper *oldContext, contextWrapper *newContext){
-    /*
-        sd ra, 0 * 8(a0)
-        sd sp, 1 * 8(a0)
-
-        ld ra, 0 * 8(a1)
-        ld sp, 1 * 8(a1)
-    */
     if(oldContext != nullptr){
     // oldContext->pc = pc;
-    __asm__ volatile ("sd sp, 8(a0)");
-    __asm__ volatile ("sd ra, 0(a0)");
-    // __asm__ volatile("mv %0, s0" : "=r"(oldContext->s[0]));
-    // __asm__ volatile("mv %0, s1" : "=r"(oldContext->s[1]));
-    // __asm__ volatile("mv %0, s2" : "=r"(oldContext->s[2]));
-    // __asm__ volatile("mv %0, s3" : "=r"(oldContext->s[3]));
-    // __asm__ volatile("mv %0, s4" : "=r"(oldContext->s[4]));
-    // __asm__ volatile("mv %0, s5" : "=r"(oldContext->s[5]));
-    // __asm__ volatile("mv %0, s6" : "=r"(oldContext->s[6]));
-    // __asm__ volatile("mv %0, s7" : "=r"(oldContext->s[7]));
-    // __asm__ volatile("mv %0, s8" : "=r"(oldContext->s[8]));
-    // __asm__ volatile("mv %0, s9" : "=r"(oldContext->s[9]));
-    // __asm__ volatile("mv %0, s10" : "=r"(oldContext->s[10]));
-    // __asm__ volatile("mv %0, s11" : "=r"(oldContext->s[11]));
+    __asm__ volatile ("sd ra, 0 * 8(a0)");
+    __asm__ volatile ("sd sp, 1 * 8(a0)");
+
+    __asm__ volatile ("sd s0, 2 * 8(a0)");
+    __asm__ volatile ("sd s1, 3 * 8(a0)");
+    __asm__ volatile ("sd s2, 4 * 8(a0)");
+    __asm__ volatile ("sd s3, 5 * 8(a0)");
+    __asm__ volatile ("sd s4, 6 * 8(a0)");
+    __asm__ volatile ("sd s5, 7 * 8(a0)");
+    __asm__ volatile ("sd s6, 8 * 8(a0)");
+    __asm__ volatile ("sd s7, 9 * 8(a0)");
+    __asm__ volatile ("sd s8, 10 * 8(a0)");
+    __asm__ volatile ("sd s9, 11 * 8(a0)");
+    __asm__ volatile ("sd s10, 12 * 8(a0)");
+    __asm__ volatile ("sd s11, 13 * 8(a0)");
     }
     if(newContext->sp != 0)
         __asm__ volatile ("ld sp, 8(a1)");
     __asm__ volatile ("ld ra, 0(a1)");
-    // __asm__ volatile("mv s0, %0" :: "r"(oldContext->s[0]));
-    // __asm__ volatile("mv s1, %0" :: "r"(oldContext->s[1]));
-    // __asm__ volatile("mv s2, %0" :: "r"(oldContext->s[2]));
-    // __asm__ volatile("mv s3, %0" :: "r"(oldContext->s[3]));
-    // __asm__ volatile("mv s4, %0" :: "r"(oldContext->s[4]));
-    // __asm__ volatile("mv s5, %0" :: "r"(oldContext->s[5]));
-    // __asm__ volatile("mv s6, %0" :: "r"(oldContext->s[6]));
-    // __asm__ volatile("mv s7, %0" :: "r"(oldContext->s[7]));
-    // __asm__ volatile("mv s8, %0" :: "r"(oldContext->s[8]));
-    // __asm__ volatile("mv s9, %0" :: "r"(oldContext->s[9]));
-    // __asm__ volatile("mv s1, %0" :: "r"(oldContext->s[10]));
-    // __asm__ volatile("mv s1, %0" :: "r"(oldContext->s[11]));
+    __asm__ volatile ("ld s0, 2 * 8(a1)");
+    __asm__ volatile ("ld s1, 3 * 8(a1)");
+    __asm__ volatile ("ld s2, 4 * 8(a1)");
+    __asm__ volatile ("ld s3, 5 * 8(a1)");
+    __asm__ volatile ("ld s4, 6 * 8(a1)");
+    __asm__ volatile ("ld s5, 7 * 8(a1)");
+    __asm__ volatile ("ld s6, 8 * 8(a1)");
+    __asm__ volatile ("ld s7, 9 * 8(a1)");
+    __asm__ volatile ("ld s8, 10 * 8(a1)");
+    __asm__ volatile ("ld s9, 11 * 8(a1)");
+    __asm__ volatile ("ld s10, 12 * 8(a1)");
+    __asm__ volatile ("ld s11, 13 * 8(a1)");
     return;
 }
