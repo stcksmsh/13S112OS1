bcd69f28aa444469f6b34fee97d9c730564e491b
stcksmsh <vukicevickosta@gmail.com>
2023-05-30 01:59:41 +0200

    ... i am a dumbass ahahaha ...

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,10 @@ void usermainWrapper(void* arg){
     usermain();
 }
 
-int testing = 0;
-
 void main(){
     __asm__ volatile ("csrw stvec, %0" : :  "r"(&trap));
     changeUser();
     thread_t handle;
-    while(testing < 10)putc('0' + testing);
     thread_create(&handle, nullptr, nullptr);//    <-------------------
     thread_create(&handle, usermainWrapper, nullptr);//                    |
     thread::running = Scheduler::get(); // the nullptr nullptr one  ---
