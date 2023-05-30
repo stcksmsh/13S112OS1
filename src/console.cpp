94cff1ab44199885983c214de6919d295bf8084f
stcksmsh <vukicevickosta@gmail.com>
2023-05-30 02:43:06 +0200

    something??

--- src/console.cpp
+++ src/console.cpp
@@ -1,8 +1,10 @@
 #include "../h/console.h"
 #include "../h/syscall_c.hpp"
 
-char* Console::outBuffer = nullptr;
-char* Console::inBuffer = nullptr;
+char* Console::outBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
+char* Console::inBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
+int Console::inBufferIndex = -1;
+int Console::outBufferIndex = -1;
 
 char Console::read(){
     if(inBufferIndex < 0)return -1;
@@ -15,15 +17,15 @@ void Console::write(char c){
 
 
 void Console::console_handler(){
-    if(outBuffer == nullptr){
-        outBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
-        outBufferIndex = -1;
-    }
+    // if(outBuffer == nullptr){
+    //     outBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
+    //     outBufferIndex = -1;
+    // }
 
-    if(inBuffer == nullptr){
-        inBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
-        inBufferIndex = -1;
-    }
+    // if(inBuffer == nullptr){
+    //     inBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
+    //     inBufferIndex = -1;
+    // }
 
     while(((*(char*)CONSOLE_STATUS) & CONSOLE_RX_STATUS_BIT )&& inBufferIndex < BUFFER_SIZE){
         inBuffer[inBufferIndex++] = (char)CONSOLE_RX_DATA;
