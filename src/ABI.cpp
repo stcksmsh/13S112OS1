73d6a6b88c9729ce74bdb51c209d7c9e2661a8bf
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 10:21:10 +0200

    STABLE:     MEM_ALLOC     MEM_FREE     THREAD_CREATE     THREAD_EXIT     THREAD_DISPATCH     SEM_OPEN     SEM_CLOSE     SEM_WAIT     SEM_SIGNAL     TIME SHARING NEEDS WORK:     THREAD_SLEEP NEEDS A LOT OF WORK:     PUTC     GETC NOT STARTED:     C++ API

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -180,7 +180,6 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
     }
     else if (scause== 0x8000000000000009UL)
     {   
-        __putc('h');
         // int irq = plic_claim();
         // if(irq == CONSOLE_IRQ){
         // }
--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,6 @@ void usermainWrapper(void* arg){
 void main(){
     __asm__ volatile ("csrw stvec, %0" : :  "r"(&trap)); // sets the syscall routine
     changeUser();
-    // Console::write('.');
-    // putc('t');
     thread_t handle;
     thread_create(&handle, nullptr, nullptr);// <----------------------7
     thread::running = Scheduler::get(); // the nullptr nullptr one ---/
