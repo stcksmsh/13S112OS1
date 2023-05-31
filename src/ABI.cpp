49a6db77a63930dfef39b6b0ebce943118729dd7
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 09:58:03 +0200

    ..

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -45,7 +45,9 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
     __asm__ volatile ("csrr %0, sepc" : "=r" (sepc));
     uint64 volatile sstatus = sstatusRead();
     sstatus |= 1<<5;
-    __asm__ volatile ("csrw sie, %0" : : "r"(1<<1 | 1<<9));// allows for software interrupts
+    uint64 sie;
+    __asm__ volatile ("csrr %0, sie" : "=r"(sie));
+    __asm__ volatile ("csrw sie, %0" : : "r"(sie | 1<<1 | 1<<9));// allows for software interrupts
     // User and Supervisor syscalls
     if (scause == 0x0000000000000009UL || scause == 0x0000000000000008UL)
     {
