8c2ea8f15eafdcfbd0af7460aecfe9019d858e6f
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 08:56:46 +0200

    commented out whole of console.cpp

--- src/console.cpp
+++ src/console.cpp
@@ -1,29 +1,29 @@
-#include "../h/console.hpp"
-#include "../h/syscall_c.hpp"
+// #include "../h/console.hpp"
+// #include "../h/syscall_c.hpp"
 
-char* Console::outBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
-char* Console::inBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
-int Console::inBufferIndex = -1;
-int Console::outBufferIndex = -1;
+// char* Console::outBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
+// char* Console::inBuffer = (char*)mem_alloc(sizeof(char) * BUFFER_SIZE);
+// int Console::inBufferIndex = -1;
+// int Console::outBufferIndex = -1;
 
-char Console::read(){
-    if(inBufferIndex < 0)return -1;
-    return inBuffer[inBufferIndex--];
-}
+// char Console::read(){
+//     if(inBufferIndex < 0)return -1;
+//     return inBuffer[inBufferIndex--];
+// }
 
-void Console::write(char c){
-    outBuffer[outBufferIndex++] = c;
-}
+// void Console::write(char c){
+//     outBuffer[outBufferIndex++] = c;
+// }
 
 
-void Console::console_handler(){
+// void Console::console_handler(){
 
-    while(((*(char*)CONSOLE_STATUS) & CONSOLE_RX_STATUS_BIT )&& inBufferIndex < BUFFER_SIZE){
-        inBuffer[inBufferIndex++] = (char)CONSOLE_RX_DATA;
-    }
+//     while(((*(char*)CONSOLE_STATUS) & CONSOLE_RX_STATUS_BIT )&& inBufferIndex < BUFFER_SIZE){
+//         inBuffer[inBufferIndex++] = (char)CONSOLE_RX_DATA;
+//     }
 
-    while(((*(char*)CONSOLE_STATUS) & CONSOLE_TX_STATUS_BIT) && outBufferIndex>=0){
-        (*(char*)CONSOLE_TX_DATA) = outBuffer[outBufferIndex--];
-    }
+//     while(((*(char*)CONSOLE_STATUS) & CONSOLE_TX_STATUS_BIT) && outBufferIndex>=0){
+//         (*(char*)CONSOLE_TX_DATA) = outBuffer[outBufferIndex--];
+//     }
 
-}
+// }
