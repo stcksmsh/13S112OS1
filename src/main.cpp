678dd4212f0dfc918a6c862af6dca7ceff8f5433
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 08:02:54 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,12 @@ void main(){
     thread_create(&handle, nullptr, nullptr);// <----------------------7
     thread_create(&handle, usermainWrapper, nullptr);//               /
     thread::running = Scheduler::get(); // the nullptr nullptr one --/
-    while(!Scheduler::isEmpty()){
-        thread_dispatch();
+    do{
+        while(!Scheduler::isEmpty()){
+            thread_dispatch();
+        }
     }
-    if(thread::sleepHead != nullptr)__putc('\n');
+    while(thread::sleepHead != nullptr);
     uint64 t = thread::time;
     __putc('0' + t/1000);
     __putc('0' + (t/100)%10);
