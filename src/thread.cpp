63cac663f2cd92fdc7fbb7078d65f6240ac265bc
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 06:33:18 +0200

    ?????

--- src/thread.cpp
+++ src/thread.cpp
@@ -122,7 +122,7 @@ void thread::wake(){
 
 void thread::dispatch(){
     thread_t oldThread = running;
-    if(oldThread!=nullptr && oldThread->start_routine!=nullptr && !oldThread->finished && !oldThread->blocked && !oldThread->sleeping)Scheduler::put(running);
+    if(oldThread!=nullptr && !oldThread->finished && !oldThread->blocked && !oldThread->sleeping)Scheduler::put(running);
     running->timeLeftToRun = DEFAULT_TIME_SLICE;
     running = Scheduler::get();
     if(running == nullptr){
@@ -134,8 +134,6 @@ void thread::dispatch(){
         if(newThread != nullptr){
             Scheduler::put(running);
             running = newThread;
-        }else if(sleepHead!=nullptr){
-            Scheduler::put(running);
         }
     }
 
