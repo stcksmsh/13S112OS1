2ec9e7e606472aaaccaf04dfb125bd970586e5b6
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 08:59:19 +0200

    dumbasss

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,6 @@ void usermainWrapper(void* arg){
 void main(){
     __asm__ volatile ("csrw stvec, %0" : :  "r"(&trap)); // sets the syscall routine
     changeUser();
-    *thread::time = 1;
-    __putc('0' + *thread::time);
-    return;
     thread_t handle;
     thread_create(&handle, nullptr, nullptr);// <----------------------7
     thread_create(&handle, usermainWrapper, nullptr);//               /
