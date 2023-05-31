95c6c42f8694316ed0132a990257d7a7782c7fce
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 09:31:46 +0200

    ...

--- src/thread.cpp
+++ src/thread.cpp
@@ -80,8 +80,8 @@ void thread::joinTo(){/// thread1.join() is the same as invoking thread_join(thr
     joinList *node = (joinList*)MemoryAllocator::getInstance().mem_alloc((sizeof(joinList) + MEM_BLOCK_SIZE - 1)/MEM_BLOCK_SIZE);
     node->handle = running;
     node->next = nullptr;
-        return;
     if(joinTail == nullptr){
+    return;
         joinHead = node;
         joinTail = node;
     }else{
