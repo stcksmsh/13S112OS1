f61aee603fc4eb5ca97eba1c62de024827b8d582
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 09:43:36 +0200

    ss

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -180,14 +180,13 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
     }
     else if (scause== 0x8000000000000009UL)
     {   
-        int irq = plic_claim();
-        if(irq == CONSOLE_IRQ){
-            Console::console_handler();
-        }
-        plic_complete(irq);
+        // int irq = plic_claim();
+        // if(irq == CONSOLE_IRQ){
+        //     Console::console_handler();
+        // }
+        // plic_complete(irq);
         // interrupt: yes; cause code: supervisor external interrupt (PLIC; could be keyboard)
-        // console_handler();
-        // Console::console_handler();
+        console_handler();
     }
     else if (scause == 0x0000000000000002UL){
         __putc('\n');
