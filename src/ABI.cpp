b0bfcfeb4596f872474e9b9c0f5c08986f4a68cd
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 02:02:59 +0200

    ?

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -149,7 +149,7 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
         if(!thread::running->live()){/// it has run for longer than its alloted time slice
             thread::dispatch();
         }
-        sepc-=4;
+        // sepc-=4;
     }
     else if (scause== 0x8000000000000009UL)
     {
