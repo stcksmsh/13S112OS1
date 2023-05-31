bb80699d671faeee517a246936056748b992a84a
stcksmsh <vukicevickosta@gmail.com>
2023-05-31 09:55:52 +0200

    ..

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -181,8 +181,8 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
     else if (scause== 0x8000000000000009UL)
     {   
         int irq = plic_claim();
-        if(irq == CONSOLE_IRQ){
             __putc('x');
+        if(irq == CONSOLE_IRQ){
             Console::console_handler();
         }
         plic_complete(irq);
