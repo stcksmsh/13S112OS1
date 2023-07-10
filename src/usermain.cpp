e39cd84701187837be683f4fe0ff412c19149798
stcksmsh <vukicevickosta@gmail.com>
2023-07-11 00:04:07 +0200

    debugging

--- src/usermain.cpp
+++ src/usermain.cpp
@@ -4,12 +4,18 @@
 void usermain(){
     int **arr;
     arr = (int**)mem_alloc(sizeof(int*) * 100);
+    putc('1');
+    putc('\n');
     for(int i = 0; i < 100; i ++){
+        putc('2');
+        putc('\n');
         arr[i] = (int*)mem_alloc(sizeof(int) * 100);
         for(int j = 0; j < 100; j ++){
             arr[i][j] = i*100+j;
         }
     }
+    putc('3');
+    putc('\n');
 
     for(int i = 0; i < 100; i ++){
         for(int j = 0; j < 100; j ++){
@@ -17,5 +23,7 @@ void usermain(){
         }
         putc('\n');
     }
+    putc('4');
+    putc('\n');
 
 }
