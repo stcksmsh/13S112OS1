dd6f39028b8edf39ba1d4265fa4a244ff9d01943
stcksmsh <vukicevickosta@gmail.com>
2023-07-10 18:22:56 +0200

    fixed test

--- src/userMain.cpp
+++ src/userMain.cpp
@@ -7,25 +7,25 @@
 
 #if LEVEL_2_IMPLEMENTED == 1
 // TEST 1 (zadatak 2, niti C API i sinhrona promena konteksta)
-#include "../test/Threads_C_API_test.hpp"
+#include "../h/Threads_C_API_test.hpp"
 // TEST 2 (zadatak 2., niti CPP API i sinhrona promena konteksta)
-#include "../test/Threads_CPP_API_test.hpp"
+#include "../h/Threads_CPP_API_test.hpp"
 // TEST 7 (zadatak 2., testiranje da li se korisnicki kod izvrsava u korisnickom rezimu)
-#include "../test/System_Mode_test.hpp"
+#include "../h/System_Mode_test.hpp"
 #endif
 
 #if LEVEL_3_IMPLEMENTED == 1
 // TEST 3 (zadatak 3., kompletan C API sa semaforima, sinhrona promena konteksta)
-#include "../test/ConsumerProducer_C_API_test.hpp"
+#include "../h/ConsumerProducer_C_API_test.hpp"
 // TEST 4 (zadatak 3., kompletan CPP API sa semaforima, sinhrona promena konteksta)
-#include "../test/ConsumerProducer_CPP_Sync_API_test.hpp"
+#include "../h/ConsumerProducer_CPP_Sync_API_test.hpp"
 #endif
 
 #if LEVEL_4_IMPLEMENTED == 1
 // TEST 5 (zadatak 4., thread_sleep test C API)
-#include "../test/ThreadSleep_C_API_test.hpp"
+#include "../h/ThreadSleep_C_API_test.hpp"
 // TEST 6 (zadatak 4. CPP API i asinhrona promena konteksta)
-#include "../test/ConsumerProducer_CPP_API_test.hpp"
+#include "../h/ConsumerProducer_CPP_API_test.hpp"
 #include "../h/System_Mode_test.hpp"
 
 #endif
