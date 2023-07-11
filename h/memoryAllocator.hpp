f7a1cd37d9312be26df174be8d0a72d6f0dca7a9
stcksmsh <vukicevickosta@gmail.com>
2023-07-12 00:09:53 +0200

    how bout with foreign memoryAllocator?

--- h/memoryAllocator.hpp
+++ h/memoryAllocator.hpp
@@ -1,25 +1,22 @@
-#ifndef MEMORYALLOCATORHEADERFILE
-#define MEMORYALLOCATORHEADERFILE
+//
+// Created by os on 8/6/22.
+//
 
+#ifndef PROJECT_BASE_V1_1_MEMALLOCATOR_H
+#define PROJECT_BASE_V1_1_MEMALLOCATOR_H
 #include "../lib/hw.h"
 struct FreeMemorySegment{
-    FreeMemorySegment* prevSegment;
-    FreeMemorySegment* nextSegment;
-    size_t segmentSize;
+    FreeMemorySegment* next;
+    FreeMemorySegment* prev;
+    size_t size;
 };
 class MemoryAllocator {
+    FreeMemorySegment *head=nullptr;
+    void tryToJoin(FreeMemorySegment* cur);
 public:
     static MemoryAllocator& getInstance();
-private:
-    MemoryAllocator(){};
-    MemoryAllocator( MemoryAllocator const& ) = delete;
-    MemoryAllocator& operator=( MemoryAllocator const& ) = delete;
-    FreeMemorySegment *head = nullptr;
-    void attemptMerge( FreeMemorySegment* );
-public:
-    void *mem_alloc( size_t );
-    int mem_free( void* );
-    int numberOfSegments();
-};
+    void *mem_alloc(size_t sz);
+    int mem_free(void* adr);
 
-#endif
+};
+#endif //PROJECT_BASE_V1_1_MEMALLOCATOR_H
