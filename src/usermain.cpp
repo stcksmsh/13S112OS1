dbbb40dbbb972d3e188de574054803083c7af401
stcksmsh <vukicevickosta@gmail.com>
2023-07-11 20:03:41 +0200

    assdumb

--- src/usermain.cpp
+++ src/usermain.cpp
@@ -2,23 +2,28 @@
 #include "../h/usermain.hpp"
 #include "../h/memoryAllocator.hpp"
 
+void segPrint(){
+    int sgmntcnt = MemoryAllocator::getInstance().numberOfSegments();
+    while(sgmntcnt > 0){
+        putc('0' + sgmntcnt%10);
+        sgmntcnt/=10;
+    }
+    putc('\n');
+}
+
 void usermain(){
     int **arr;
     arr = (int**)mem_alloc(sizeof(int*) * 100);
     // putc('1');
     // putc('\n');
+    segPrint();
     for(int i = 0; i < 100; i ++){
         putc('2');
         arr[i] = (int*)mem_alloc(sizeof(int) * 100);
+        segPrint();
         for(int j = 0; j < 100 ; j++){
             arr[i][j] = 100*i + j;
         }
-        int sgmntcnt = MemoryAllocator::getInstance().numberOfSegments();
-        while(sgmntcnt > 0){
-            putc('0' + sgmntcnt%10);
-            sgmntcnt/=10;
-        }
-        putc('\n');
     }
     // putc('3');
     // putc('\n');
