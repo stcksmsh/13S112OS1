c7b320826e24f339e5983ffe28ec80a2c9edbf02
stcksmsh <vukicevickosta@gmail.com>
2023-07-11 20:15:29 +0200

    idk man

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@ void usermainWrapper(void* arg){
 void main(){
     __asm__ volatile ("csrw stvec, %0" : :  "r"(&trap)); // sets the syscall routine
     changeUser();
+    putc('0');
+    putc('\n');
     thread_t handle;
     thread_create(&handle, nullptr, nullptr);// <----------------------7
     thread::running = Scheduler::get(); // the nullptr nullptr one ---/2
