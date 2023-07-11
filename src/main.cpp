29fd26177351940b30457748d612c6c3c8789a89
stcksmsh <vukicevickosta@gmail.com>
2023-07-11 20:01:51 +0200

    usermain no longer runs in thread

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@ void usermainWrapper(void* arg){
 void main(){
     __asm__ volatile ("csrw stvec, %0" : :  "r"(&trap)); // sets the syscall routine
     changeUser();
+    usermainWrapper;
+    return;
     thread_t handle;
     thread_create(&handle, nullptr, nullptr);// <----------------------7
     thread::running = Scheduler::get(); // the nullptr nullptr one ---/2
