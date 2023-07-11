13934b65eaaa8ec49dfd475b055e2938ed3372d5
stcksmsh <vukicevickosta@gmail.com>
2023-07-12 00:08:01 +0200

    im stupido

--- h/memoryAllocator.hpp
+++ h/memoryAllocator.hpp
@@ -1,22 +1,25 @@
-//
-// Created by os on 8/6/22.
-//
+#ifndef MEMORYALLOCATORHEADERFILE
+#define MEMORYALLOCATORHEADERFILE
 
-#ifndef PROJECT_BASE_V1_1_MEMALLOCATOR_H
-#define PROJECT_BASE_V1_1_MEMALLOCATOR_H
 #include "../lib/hw.h"
 struct FreeMemorySegment{
-    FreeMemorySegment* next;
-    FreeMemorySegment* prev;
-    size_t size;
+    FreeMemorySegment* prevSegment;
+    FreeMemorySegment* nextSegment;
+    size_t segmentSize;
 };
 class MemoryAllocator {
-    FreeMemorySegment *head=nullptr;
-    void tryToJoin(FreeMemorySegment* cur);
 public:
     static MemoryAllocator& getInstance();
-    void *mem_alloc(size_t sz);
-    int mem_free(void* adr);
-
+private:
+    MemoryAllocator(){};
+    MemoryAllocator( MemoryAllocator const& ) = delete;
+    MemoryAllocator& operator=( MemoryAllocator const& ) = delete;
+    FreeMemorySegment *head = nullptr;
+    void attemptMerge( FreeMemorySegment* );
+public:
+    void *mem_alloc( size_t );
+    int mem_free( void* );
+    int numberOfSegments();
 };
-#endif //PROJECT_BASE_V1_1_MEMALLOCATOR_H
+
+#endif
--- src/MemoryAllocator.cpp
+++ src/MemoryAllocator.cpp
@@ -1,74 +1,83 @@
 #include "../h/memoryAllocator.hpp"
 
-void *MemoryAllocator::mem_alloc(size_t sz) {
-    static bool setH=false;
-    if(!setH){
-        setH=true;
-        head=(FreeMemorySegment*)((uint64 *)HEAP_START_ADDR);
-        head->next=nullptr;
-        head->prev=nullptr;
-        head->size=((uint64)((uint64 *)HEAP_END_ADDR-(uint64 *)HEAP_START_ADDR)-sizeof(MemoryAllocator))/MEM_BLOCK_SIZE;
+void *MemoryAllocator::mem_alloc( size_t size ){
+    static bool headInitialised = false;
+    if(!headInitialised){
+        headInitialised = true;
+        head = (FreeMemorySegment*)((uint64)HEAP_START_ADDR);
+        head->prevSegment = nullptr;
+        head->nextSegment = nullptr;
+        head->segmentSize = ((size_t)( (uint64)HEAP_END_ADDR - (uint64)HEAP_START_ADDR ) - sizeof(MemoryAllocator) ) / MEM_BLOCK_SIZE;
     }
-    void* ret= nullptr;
-    for(FreeMemorySegment* tmp=head;tmp!=nullptr;tmp=tmp->next){
-        if(tmp->size>=sz){
-            ret=tmp;
-            //ako je ostalo manje od velicine bloka izbacujem iz liste
-            if(tmp->size-sz==0){
-                if(tmp->prev!=nullptr)tmp->prev->next = tmp->next;
-                else  head=tmp->next;
-                if(tmp->next!=nullptr)tmp->next->prev =tmp->prev;
-                tmp->size = sz;
-                break;
-            }
-            //ako je ostalo vise od jednog bloka
-            FreeMemorySegment* newNode=(FreeMemorySegment*)((uint64 *)tmp+sz*MEM_BLOCK_SIZE);
-            if(tmp->prev)tmp->prev->next=newNode;
-            else head=newNode;
-            if(tmp->next)tmp->next->prev=newNode;
-            newNode->prev=tmp->prev;
-            newNode->next=tmp->next;
-            newNode->size=tmp->size-sz;
-            tmp->size = sz;
-            break;
-        }
+    FreeMemorySegment* freeSegment = head;
+    while(freeSegment != nullptr && freeSegment->segmentSize < size)freeSegment = freeSegment->nextSegment;
+    if(freeSegment == nullptr || freeSegment->segmentSize < size)return nullptr;
+    if(freeSegment->segmentSize == size){
+        if(freeSegment->prevSegment) freeSegment->prevSegment->nextSegment = freeSegment->nextSegment;
+        else head = freeSegment->nextSegment;
+        if(freeSegment->nextSegment != nullptr) freeSegment->nextSegment->prevSegment = freeSegment->prevSegment;
+        return (void*)((uint64)freeSegment + MEM_BLOCK_SIZE);
     }
-    return (uint64 *)ret+sizeof(FreeMemorySegment);
+    FreeMemorySegment* newSegment = (FreeMemorySegment*)((uint64)freeSegment + size * MEM_BLOCK_SIZE + MEM_BLOCK_SIZE);
+    newSegment->segmentSize = freeSegment->segmentSize - size;
+    newSegment->prevSegment = freeSegment->prevSegment;
+    newSegment->nextSegment = freeSegment->nextSegment;
+    
+    if(newSegment->nextSegment != nullptr){
+        newSegment->nextSegment->prevSegment = newSegment;
+        attemptMerge(newSegment);
+    }
+    if(newSegment->prevSegment != nullptr)  newSegment->prevSegment->nextSegment = newSegment;
+    else head = newSegment;
+    freeSegment->segmentSize = size;
+    return (void*)((uint64)freeSegment + MEM_BLOCK_SIZE);
 }
 
 MemoryAllocator& MemoryAllocator::getInstance() {
-    static MemoryAllocator singleton;
-    return singleton;
+    static MemoryAllocator instance;
+    return instance;
 }
 
-void MemoryAllocator::tryToJoin(FreeMemorySegment *cur) {
-
-    if(cur->next&& (char*)(cur->next)==((char*)cur+cur->size*MEM_BLOCK_SIZE)){
-        cur->size+=cur->next->size;
-        cur->next=cur->next->next;
-        if(cur->next)cur->next->prev=cur;
-    }
-
+void MemoryAllocator::attemptMerge( FreeMemorySegment *segment ) {
+    if((uint64)segment->nextSegment != (uint64)segment + (segment->segmentSize + 1)*MEM_BLOCK_SIZE)
+        return;
+    segment->segmentSize += segment->nextSegment->segmentSize;
+    segment->nextSegment = segment->nextSegment->nextSegment;
+    if(segment->nextSegment) segment->nextSegment->prevSegment = segment;
 }
 
-int MemoryAllocator::mem_free(void *adr) {
-    if((uint64 *)adr>(uint64*)HEAP_END_ADDR || (uint64 *)adr<(uint64 *)HEAP_START_ADDR)return -1;
-    FreeMemorySegment* prev=0;
-    if(head==nullptr || (char*)adr<(char*)head) prev=0;
-    else{
-        for(prev=head;prev->next!=0 && (char*)adr>(char*)(prev->next);prev=prev->next);
+int MemoryAllocator::numberOfSegments(){
+    int count = 0;
+    FreeMemorySegment *segment = head;
+    while(segment){
+        count ++;
+        segment = segment->nextSegment;
     }
-    FreeMemorySegment* seg = (FreeMemorySegment*) ((uint64*)adr - sizeof(FreeMemorySegment));
-    seg->prev=prev;
-    if(prev)seg->next=prev->next;
-    else {
-        seg->next = head;
+    return count;
+}
+
+int MemoryAllocator::mem_free( void *address ) {
+    if((uint64)address >= (uint64)HEAP_END_ADDR || (uint64)address < (uint64)HEAP_START_ADDR)
+        return -1;
+    FreeMemorySegment* previousSegment = nullptr;
+    if(head && head->nextSegment < address){
+        previousSegment = head;
+        while(previousSegment->nextSegment != nullptr && previousSegment->nextSegment < address)
+            previousSegment = previousSegment->nextSegment;
     }
-    if(seg->next)seg->next->prev=seg;
-    if(prev)prev->next=seg;
-    else head=seg;
-    if(seg!=nullptr)tryToJoin(seg);
-    if(prev!=nullptr)tryToJoin(prev);
+
+    FreeMemorySegment* newSegment = (FreeMemorySegment*)((uint64)address - MEM_BLOCK_SIZE);
+    newSegment->prevSegment = previousSegment;
+    if(previousSegment){
+        if(previousSegment->nextSegment)
+            previousSegment->nextSegment->prevSegment = newSegment;
+        newSegment->nextSegment = previousSegment->nextSegment;
+        previousSegment->nextSegment = newSegment;
+    }else{
+        newSegment->nextSegment = head;
+        head = newSegment;
+    }
+    if(previousSegment)attemptMerge(previousSegment);
+    attemptMerge(newSegment);
     return 0;
 }
-
