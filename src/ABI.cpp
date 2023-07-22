39e89c0a3bc20d921ebada98f3d68028095d4978
stcksmsh <vukicevickosta@gmail.com>
2023-07-22 21:54:41 +0200

    asd

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -180,7 +180,7 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
         /// next we wake the sleeping threads;
         threadSleepHandler::wake();
         /// and finally we test for preemption
-        // if(!thread::running->live())thread::dispatch();
+        if(!thread::running->live())thread::dispatch();
         __asm__ volatile ("csrw sepc, %0" : : "r" (sepc));
         sstatusWrite(sstatus);
         sipBitClear(1);
