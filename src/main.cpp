75f4d254438b30f350b90a65381e9eb787d754ba
stcksmsh <vukicevickosta@gmail.com>
2023-07-22 20:52:26 +0200

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,13 @@ void usermainWrapper(void* arg){
 void main(){
     __asm__ volatile ("csrw stvec, %0" : :  "r"(&trap)); // sets the syscall routine
     putc('m');
-    // changeUser();
-    // putc('1');
+    changeUser();
+    putc('\n');
     thread_t out, main, prog;
     thread_create(&main, nullptr, nullptr);
-    // putc('2');
     thread::running = Scheduler::get();
-    // putc('3');
     thread_create(&out, Console::outThread, nullptr);
-    // putc('4');
-    // Scheduler::setCore(out, main);
-    // putc('5');
     thread_create(&prog, usermainWrapper, nullptr);
-    // putc('6');   
     do{
         while(!Scheduler::isEmpty())thread_dispatch();
     }while(!Scheduler::isEmpty() || !threadSleepHandler::allAwake());
