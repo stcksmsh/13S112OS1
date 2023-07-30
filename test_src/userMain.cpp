5790bbe239f1e1ad54ef30d830718c333418cf01
stcksmsh <vukicevickosta@gmail.com>
2023-07-30 17:16:43 +0200

    is it reading properly?

--- test_src/userMain.cpp
+++ test_src/userMain.cpp
@@ -34,7 +34,7 @@ void userMain() {
     printString("Unesite broj testa? [1-7]\n");
     int test = getc() - '0';
     getc(); // Enter posle broja
-
+    putc(test);
     if ((test >= 1 && test <= 2) || test == 7) {
         if (LEVEL_2_IMPLEMENTED == 0) {
             printString("Nije navedeno da je zadatak 2 implementiran\n");
