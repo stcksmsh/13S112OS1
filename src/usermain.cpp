a187389f77505682ae6b2343407d4c8b796ac970
stcksmsh <vukicevickosta@gmail.com>
2023-07-31 23:16:13 +0200

    removed the sem

--- src/usermain.cpp
+++ src/usermain.cpp
@@ -2,7 +2,7 @@
 #include "../h/usermain.hpp"
 #include "../h/thread.hpp"
 
-sem_t sem1;
+// sem_t sem1;
 // sem_t sem2;
 
 void printInt(int i){
@@ -35,7 +35,7 @@ void thread_test_1(void* args){
     thread_sleep(50);
     putc('1');
     putc('S');
-    sem_signal(sem1);
+    // sem_signal(sem1);
 }
 void thread_test_2(void* args){
     // for(int i = 0;i < 1000000000; i ++){} /// busy wait
@@ -49,7 +49,7 @@ void thread_test_2(void* args){
 
 void usermain(){
     thread_t t1, t2;
-    sem_open(&sem1, 0);
+    // sem_open(&sem1, 0);
     // sem_open(&sem2, 0);
     thread_create(&t1, thread_test_1, nullptr);
     thread_create(&t2, thread_test_2, nullptr);
@@ -61,5 +61,5 @@ void usermain(){
     putc('4');
     if(getc() == 'a')putc('x');
     putc('5');
-    sem_wait(sem1);
+    // sem_wait(sem1);
 }
