4443e5ad45962b3155ac1a5c8c8ad593ab57abe7
stcksmsh <vukicevickosta@gmail.com>
2023-07-31 20:21:12 +0200

    asd

--- src/console.cpp
+++ src/console.cpp
@@ -57,11 +57,11 @@ void Console::write(char ch){
 void Console::outThread(void* arg){
     Console *c = getInstance();
     do{
-        // __putc('c');
+        putc('c');
         while((((char*)CONSOLE_STATUS)[0] & CONSOLE_TX_STATUS_BIT) > 0 && !c->outBuffer.isEmpty()){
             ((char*)CONSOLE_TX_DATA)[0] = c->outBuffer.get();
         }
-        // __putc('e');
+        putc('e');
         // while((((char*)CONSOLE_STATUS)[0] & CONSOLE_TX_STATUS_BIT) > 0 && !c->outBuffer.isEmpty())__putc(c->outBuffer.get());
         thread_dispatch();
     }while(c->running || !c->outBuffer.isEmpty());
