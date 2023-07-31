f80076a5b6a3b4e8a4663fc70ebf2f3573469f52
stcksmsh <vukicevickosta@gmail.com>
2023-07-31 23:13:04 +0200

    HELP

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ void usermainWrapper(void* arg){
 void main(){
     __asm__ volatile ("csrw stvec, %0" : :  "r"(&trap)); // sets the syscall routine
     uint64 sie;
-    __asm__ volatile ("csrr sie, %0" : "=r"(sie)); /// sets bit 1 of sie, enables interrupts
+    __asm__ volatile ("csrr %0, sie" : "=r"(sie)); /// sets bit 1 of sie, enables interrupts
     while(sie > 0){
         putc('0' + sie%10);
         sie /= 10;
