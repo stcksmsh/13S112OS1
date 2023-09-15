ee4981f3136f8a68f58d95d73b740c8e10233267
stcksmsh <vukicevickosta@gmail.com>
2023-09-15 03:29:30 +0200

    WHY IS TIMER NOT TIMING

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -184,7 +184,8 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
         sipBitClear(1);
     }
     else if (scause == 0x8000000000000001UL)
-    {
+    {   
+        putc('.');
         /// Timer
         /// first we increment the thread::time variable
         threadSleepHandler::sleepDecrement();
