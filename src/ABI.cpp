34fe3141ae037d7c452c3166100600506c75ab5f
stcksmsh <vukicevickosta@gmail.com>
2023-09-15 04:04:08 +0200

    ...

--- src/ABI.cpp
+++ src/ABI.cpp
@@ -185,7 +185,7 @@ void ABI::trapHandler() {/// address to return to (in case of c/cpp syscalls is 
     }
     else if (scause == 0x8000000000000001UL)
     {   
-        // putc('.');
+        putc('t');
         /// Timer
         /// first we decrement remaining time for the "first" sleeping thread
         threadSleepHandler::sleepDecrement();
